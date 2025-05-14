56ecd337b038421568ab1211f790a4aebdadb575
Crest <949343937@qq.com>
2025-05-14 21:26:22 +0800

    Feat modbusRtu温湿度读取

--- TWpf/TWpf/ViewModel/ModbusTemperatureViewModel.cs
+++ TWpf/TWpf/ViewModel/ModbusTemperatureViewModel.cs
@@ -16,6 +16,10 @@ namespace TWpf.ViewModel
 {
     public class TemperatureData
     {
+        public TemperatureData()
+        {
+            History = new List<TemperatureData>();
+        }
         /// <summary>
         ///  温度
         /// </summary>
@@ -26,6 +30,31 @@ namespace TWpf.ViewModel
         /// </summary>
         public decimal Humidity { get; set; }
 
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// 历史数据
+        /// </summary>
+        public List<TemperatureData> History { get; set; }
+
+        public void SetTemperature(decimal temperature, decimal humidity)
+        {
+            Temperature = temperature;
+            Humidity = humidity;
+            Time = DateTime.Now;
+        }
+
+    }
+
+    public class TempMessage
+    {
+        public string Message { get; set; }
+
+        public DateTime Time { get; set; }
+
     }
     public partial class ModbusTemperatureViewModel : ObservableObject
     {
@@ -34,10 +63,9 @@ namespace TWpf.ViewModel
         {
             _rtU = new ModbusRTUAdvance();
             _dispatcher = dispatcher;
-            TemperatureData1 = new TemperatureData
-            {
-                Humidity = 12.2m
-            };
+            TemperatureData1 = new TemperatureData();
+            TemperatureData2 = new TemperatureData();
+            Messages = new ObservableCollection<TempMessage>();
         }
         [ObservableProperty]
         private string port;
@@ -46,6 +74,10 @@ namespace TWpf.ViewModel
 
         [ObservableProperty]
         private TemperatureData temperatureData1;
+        [ObservableProperty]
+        private TemperatureData temperatureData2;
+
+        public ObservableCollection<TempMessage> Messages { get; set; }
 
         private readonly Dispatcher _dispatcher;
 
@@ -68,42 +100,69 @@ namespace TWpf.ViewModel
             if (_rtU.Connection(Port, Rate.Value, Parity.None, 8, StopBits.One))
             {
                 _cts = new CancellationTokenSource();
-                Task.Run(Communication);
-                MessageBox.Show("连接成功");
+                Task.Run(() =>
+                {
+                    Communication();
+                });
+                AddMessage("串口连接成功");
             }
             else
             {
-                MessageBox.Show("连接失败");
+                AddMessage("串口连接失败");
             }
+        }
+
+        private void AddMessage(string message)
+        {
+            Messages.Add(new TempMessage { Message = message, Time = DateTime.Now });
         }
 
         [RelayCommand]
         public void DisConnect()
         {
+            _cts.Cancel();
             _rtU.Dispose();
+            AddMessage("串口断开");
         }
+
 
         private void Communication()
         {
             while (!_cts.IsCancellationRequested)
             {
-                Task.Delay(1000);
                 try
                 {
+                    Task.Delay(1000);
                     var res = _rtU.ReadRegisters(1, 0, 2);
                     _dispatcher.Invoke(() =>
                     {
-                        TemperatureData1 = new TemperatureData
-                        {
-                            Temperature = (res.Data[0] * 256 + res.Data[1]) * 0.1M,
-                            Humidity = (res.Data[2] * 256 + res.Data[3]) * 0.1M
-                        };
+                        var temperature = (res.Data[0] * 256 + res.Data[1]) * 0.1M;
+                        var humidity = (res.Data[2] * 256 + res.Data[3]) * 0.1M;
+                        var data = new TemperatureData();
+
+                        data.SetTemperature(temperature, humidity);
+                        TemperatureData1 = data;
+                    });
+
+                    var res2 = _rtU.ReadRegisters(2, 0, 2);
+                    _dispatcher.Invoke(() =>
+                    {
+                        var temperature = (res2.Data[0] * 256 + res2.Data[1]) * 0.1M;
+                        var humidity = (res2.Data[2] * 256 + res2.Data[3]) * 0.1M;
+                        var data = new TemperatureData();
+
+                        data.SetTemperature(temperature, humidity);
+                        TemperatureData2 = data;
                     });
                 }
                 catch (Exception ex)
                 {
-
+                    _dispatcher.Invoke(() =>
+                    {
+                        AddMessage("读取异常");
+                    });
                 }
+
             }
         }
     }
