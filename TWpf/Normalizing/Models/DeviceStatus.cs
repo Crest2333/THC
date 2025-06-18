79951f6665755d9a3db7baf3652956310af2d0dc
Crest2333 <62649324+Crest2333@users.noreply.github.com>
2025-06-18 09:32:25 +0800

    Create DeviceStatus.cs

---
+++ TWpf/Normalizing/Models/DeviceStatus.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normalizing.Models
+{
+    public enum DeviceStatus
+    {
+        Normal,
+        Abnormal
+    }
+}
